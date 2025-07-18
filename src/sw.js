f09589a58c96eb841d28f0c792954826b8cadf50
Cointastig <k.pfaender1989@gmail.com>
2025-07-18 02:27:38 +0200

    Update sw.js

--- src/sw.js
+++ src/sw.js
@@ -9,7 +9,6 @@ const STATIC_ASSETS = [
   '/styles.css',
   '/app.js',
   '/manifest.json',
-  '/logo.png',
   'https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap'
 ];
 
